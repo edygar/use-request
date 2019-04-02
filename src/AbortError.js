61358a601302e08f187afe2245c1d56ad292e5fe
Edygar de Lima Oliveira <edygardelima@gmail.com>
2019-04-02 15:15:27 +0100

    Stop extending DOMError, error thrown

--- src/AbortError.js
+++ src/AbortError.js
@@ -1,9 +1,7 @@
 /**
  * Helper Class to produce local spec-compliant abortations
  */
-export default class AbortError extends ('DOMError' in global
-  ? DOMError
-  : Error) {
+export default class AbortError extends Error {
   constructor(...args) {
     super(...args)
     this.name = 'AbortError'
