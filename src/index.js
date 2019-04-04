abebf623acd6d2f5e0385e6408b3fa7635ee86dd
Edygar de Lima Oliveira <edygardelima@gmail.com>
2019-04-04 17:49:56 +0100

    Fixes export

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import useRequest from './useRequest'
+import * as cached from './cache'
 
 export default useRequest
 
-export * as cache from './cache'
+export const cache = cached
 export * from './useRequestInitiator'
 export {default as useRequestInitiator} from './useRequestInitiator'
 export {default as useRequest} from './useRequest'
